c1d06a0a382699f2c3499688ed00b97cf394313d
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-02 12:55:37 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flare = '*') {
+  return (function (adj = 'special') {
+    return `You are ${flare}${adj}${flare}!`;
+  });
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!")("a dedicated programmer");
